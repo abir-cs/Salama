ddfa663db7686e57b16b7d3a2b8564b94b658f04
rch <rachaamouri155@gmail.com>
2025-12-01 21:02:59 +0100

    little fixes

--- map.js
+++ map.js
@@ -1,11 +1,11 @@
 const mapCon = document.getElementById('map');
 if (mapCon) {
-    var map = L.map('map').setView([36.486592451438696, 6.822039510391554], 10);
+    var map = L.map('map').setView([36.36848772408414, 6.551323181538929], 10);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         // attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
-    L.marker([37.38322999945196, -2.040267971827429]).addTo(map)
+    L.marker([36.36848772408414, 6.551323181538929]).addTo(map)
         .bindPopup('Salama Clinic')
         .openPopup();
 } gi
